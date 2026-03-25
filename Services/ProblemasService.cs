54a8f80d6f10b2471e6f762cef9f6ab7f34ed2cd
Melk <melquisedequecampos.trabalho@gmail.com>
2026-03-25 16:08:50 -0300

    Atualização do Service (consta erro em ToDTo) e adição de extensions (talvez deva ser retirado depois)

--- Services/ProblemasService.cs
+++ Services/ProblemasService.cs
@@ -1,7 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CidadeAtivaApi.Data;
+using CidadeAtivaApi.DTOs;
+using CidadeAtivaApi.Models.Enum;
+using CidadeAtivaApi.Models;
+using CidadeAtivaApi.Models.Enum;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using CidadeAtivaApi.Extensions;
+
+
 
 namespace CidadeAtivaApi.Services
 {
@@ -13,7 +19,40 @@ namespace CidadeAtivaApi.Services
         // A injeção de dependencia seria tipo ao inves de criar o AppDB manualmente
         // voce recebe ele pronto pelo construtor (processo feito pelo ASP.NET)
 
+        private readonly AppDB _db; // readonly: só pode ser definido uma vez
+        public ProblemasService (AppDB db) => _db = db; // construtor simplificado
 
-        
+        // o ASP.NET cria o AppDB => Injeta no construtor => Salva em _db
+
+        // --- GET ALL ---
+        public async Task<List<RespostaProblemaDTO>> GetAllAsync(TipoProblema? tipo, StatusProblema? status)
+        {   
+            // Consulta que pode ser modificada
+            // permite montar consultas que ainda não foram executadas
+            // Serve pra ir montando a consulta aos poucos, fazendo varios tipos de filtros diferentes
+            var query = _db.Problamas.AsQueryable(); 
+
+            if (tipo.HasValue) // hasValue verifica se  a avriavel tem valor ou é null
+                query = query.Where(problema => problema.Tipo == tipo.Value);
+
+            if (status.HasValue)
+                query =  query.Where(problema => problema.Status == status.Value);
+
+            return await query
+                .OrderByDescending(problema => problema.CriadoEm)
+                .Select(problema => problema.ToDto())
+                .ToListAsync(); 
+        }
+
+            // --- GET BY ID ---
+            public async Task<RespostaProblemaDTO?> GetByIdAsync(Guid id) // guid para fazer um "unique id"
+        {
+            var problema = await _db.Problamas.FindAsync(id);
+            return problema is null ? null : ToDto(problema);
+        }
+
+            // --- CREATE ---
+
     }
 }
+
